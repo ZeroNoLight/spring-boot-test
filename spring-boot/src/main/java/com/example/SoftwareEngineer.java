f10569aacc9041f867ddcc973a1c1ca3002034e3
ZeroNoLight <moonlightandthevodka@gmail.com>
2026-01-11 17:34:39 +0200

    software engineer class basis

---
+++ spring-boot/src/main/java/com/example/SoftwareEngineer.java
@@ -0,0 +1,57 @@
+package com.example;
+
+import java.util.List;
+import java.util.Objects;
+
+public class SoftwareEngineer {
+    private Integer id;
+    private String name;
+    private List<String>techStack;
+
+    public SoftwareEngineer(Integer id, String name, List<String> techStack){
+        this.id = id;
+        this.name = name;
+        this.techStack = techStack;
+    }
+
+    public Integer getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public List<String> getTechStack(){
+        return techStack;
+    }
+
+     public void setId(Integer id){
+        this.id = id;
+    }
+
+    public void setName(){
+        this.name = name;
+    }
+
+    public void setTechStack(){
+        this.techStack = techStack;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if(o == null || getClass() != o.getClass()) return false;
+        SoftwareEngineer that = (SoftwareEngineer)o;
+        return Objects.equals(id,that.id)&&
+                Objects.equals(name,that.name)&&
+                Objects.equals(techStack, that.techStack);
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(id,name,techStack);
+    }
+
+
+
+}
